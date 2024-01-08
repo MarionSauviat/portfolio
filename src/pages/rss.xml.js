e5e5915af2e9bdf7cb5c83c61a1bf4321340bd98
MarionSauviat <marion.sauviat@etu.unilim.fr>
2024-01-08 12:24:29 +0100

    zaz

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -4,7 +4,7 @@ export async function get() {
   return rss({
     title: 'Astro Learner | Blog',
     description: "Mon voyage d'apprentissage d'Astro",
-    site: 'https://mon-site-de-blog.netlify.app',
+    site: "https://fluffy-hotteok-8cf13d.netlify.app",
     items: await pagesGlobToRssItems(import.meta.glob('./**/*.md')),
     customData: `<language>fr-fr</language>`,
   });
